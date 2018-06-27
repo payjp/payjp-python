fb5462cea3a6fda4e2c76c8efd6ebf75ed9d4efc
Shotaro Akiyama <satetsu888@gmail.com>
2018-06-27 11:29:21 +0900

    Bump version to 0.0.5

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ install_requires.append('six >= 1.9.0')
 
 setup(
     name="payjp",
-    version="0.0.4",
+    version="0.0.5",
     description='PAY.JP python bindings',
     author="PAY.JP",
     author_email='support@pay.jp',
