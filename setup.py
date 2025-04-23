950b8dec6f418298e186251aca3d1997bb823406
Yoichi Fujimoto <wozozo@gmail.com>
2025-04-23 11:01:01 +0900

    feat: add Ruff configuration for linting and formatting

--- setup.py
+++ setup.py
@@ -1,22 +1,18 @@
-import sys
-
 from setuptools import setup
 
 install_requires = []
 
-if sys.version_info < (3, 8):
-    raise DeprecationWarning('Python versions below 3.8 are no longer supported by PAY.JP. Please use Python 3.8 or higher.')
 
-install_requires.append('requests >= 2.7.0')
+install_requires.append("requests >= 2.7.0")
 
 setup(
     name="payjp",
     version="1.6.0",
-    description='PAY.JP python bindings',
+    description="PAY.JP python bindings",
     author="PAY.JP",
-    author_email='support@pay.jp',
-    packages=['payjp', 'payjp.test'],
-    url='https://github.com/payjp/payjp-python',
+    author_email="support@pay.jp",
+    packages=["payjp", "payjp.test"],
+    url="https://github.com/payjp/payjp-python",
     install_requires=install_requires,
-    python_requires='>=3.0',
+    python_requires=">=3.0",
 )
